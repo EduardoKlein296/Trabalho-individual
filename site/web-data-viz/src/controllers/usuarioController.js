ff935a00b3459b4d15ef1b1dacadb74a7ed27112
Eduardo Klein de Figueiredo <eduardo.figueiredo@sptech.school>
2025-05-30 13:00:49 -0300

    votação funcionando, na teoria

--- site/web-data-viz/src/controllers/usuarioController.js
+++ site/web-data-viz/src/controllers/usuarioController.js
@@ -8,34 +8,26 @@ function autenticar(req, res) {
     console.log("Teste emails controller" + email)
     console.log("Teste senhas controller" + senha)
 
-    if (email == undefined) {
+    if (email == undefined) { 
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
  
         usuarioModel.autenticar(email, senha)
-            .then(
+            .then( 
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                        usuarioModel.autenticar(resultadoAutenticar[0].id)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                    if (resultadoAutenticar.length > 0) {
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            email: resultadoAutenticar[0].email,
+                            nome: resultadoAutenticar[0].nome,
+                            senha: resultadoAutenticar[0].senha,
+                        });
+                                
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -90,11 +82,12 @@ function cadastrar(req, res) {
 function votacao(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var voto = req.body.votoServer;
+    var id = req.body.idUser;
 
     // Faça as validações dos valores
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.votacao(voto)
+        usuarioModel.votacao(voto, id)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- site/web-data-viz/src/models/usuarioModel.js
+++ site/web-data-viz/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT email, senha FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}' ;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -22,11 +22,11 @@ function cadastrar(nome, email, senha) {
     return database.executar(instrucaoSql);
 }
 
-function votacao(idUsuario, voto) {
+function votacao(id, voto) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO votação (fkUsuario, voto) VALUES ('${idUsuario}', '${voto}');
+        INSERT INTO votação (fkUsuario, voto) VALUES ('${id}', '${voto}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
